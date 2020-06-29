a08d4cc24df75c9860ffe9418084cfa93c5bf82a
Robert Huselius <robert@huseli.us>
2020-06-29 23:04:05 +0200

    Create Entry, save raw html etc

--- mrks/cli.py
+++ mrks/cli.py
@@ -2,22 +2,28 @@
 
 import argparse
 
-from mrks.scraper import save_current_entry
+from mrks.scraper import regenerate, scrape
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--scrape", action="store_true", help="Scrape & save results")
+    parser.add_argument("-r", "--regenerate", action="store_true", help="Regenerate entries from saved raw HTML")
 
     args = parser.parse_args()
 
-    if not args.scrape:
-        print("Nothing to do!")
-        parser.print_usage()
-    else:
-        print("Scraping & saving ...")
-        save_current_entry()
+    if args.regenerate:
+        print("Regenerating ...")
+        regenerate()
         print("... done!")
+    if args.scrape:
+        print("Scraping & saving ...")
+        scrape()
+        print("... done!")
+
+    if not args.regenerate and not args.scrape:
+        print("Nothing to do!")
+        parser.print_help()
 
 
 if __name__ == "__main__":
--- mrks/scraper.py
+++ mrks/scraper.py
@@ -1,6 +1,6 @@
 import re
 import shelve
-from datetime import date, datetime
+from datetime import datetime
 from typing import TYPE_CHECKING
 from urllib.parse import quote_plus, urljoin
 
@@ -12,7 +12,7 @@ from feedgen.feed import FeedGenerator
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 if TYPE_CHECKING:
-    from typing import List, Iterator, Dict, Union, Optional
+    from typing import List, Iterator, Dict, Union
 
 
 MRKOLL_BASE_URL = "https://mrkoll.se/"
@@ -23,56 +23,74 @@ FACEBOOK_BASE_URL = "https://www.facebook.com/search/people/?q="
 SHELVE_DB = "mrks"
 
 
-def make_search_term(name: str, location: "Optional[str]" = None) -> str:
-    search_term = name
-    if location:
-        search_term += " " + location
-    return quote_plus(search_term)
+class Entry:
+    datetime: datetime
+    raw_html: bytes
+    feed_entry: FeedEntry
+
+    @classmethod
+    def fetch_and_save(cls) -> "Entry":
+        entry = cls()
+        entry.fetch()
+        entry.generate_feed_entry()
+        entry.save()
+        return entry
+
+    @property
+    def date_str(self):
+        return str(self.datetime.date())
+
+    def __init__(self):
+        if not hasattr(self, "datetime"):
+            self.datetime = datetime.now(tz=gettz("Europe/Stockholm"))
+
+    def fetch(self):
+        self.raw_html = requests.get(SWEDEN_TOPLIST_URL).content
+
+    def generate_feed_entry(self):
+        self.feed_entry = FeedEntry()
+        self.feed_entry.title(self.date_str)
+        self.feed_entry.published(self.datetime)
+        self.feed_entry.description(self._generate_entry_html())
+
+    def save(self):
+        with shelve.open(SHELVE_DB, writeback=True) as db:
+            if "entries" not in db:
+                db["entries"] = {}
+            db["entries"][self.date_str] = self
+
+    def _extract_data(self) -> "Iterator[Dict[str, Union[str, int]]]":
+        if not hasattr(self, "raw_html"):
+            self.fetch()
+        soup = BeautifulSoup(self.raw_html, "html.parser")
+        # MrKoll's HTML is broken (the p.infoLine2 tags are not closed)
+        person_list = soup.select(".infoLine2 a")
+        for person in person_list:
+            name, location = person.select_one("span.topp1").contents
+            name = name.text
+            location = re.sub(r"^\si\s", "", location)
+            search_term = quote_plus(name)
+            result = {
+                "name": name,
+                "location": location,
+                "search_count": int(re.sub(r"\D", "", person.select_one("span.topp2").text)),
+                "mrkoll_link": urljoin(MRKOLL_BASE_URL, person.attrs["href"]),
+                "flashback_link": FLASHBACK_BASE_URL + search_term,
+                "ddg_link": DDG_BASE_URL + search_term,
+                "facebook_link": FACEBOOK_BASE_URL + search_term,
+            }
+            yield result
+
+    def _generate_entry_html(self) -> str:
+        jinja = Environment(loader=PackageLoader("mrks", "templates"), autoescape=select_autoescape(["html"]))
+        template = jinja.get_template("entry.html")
+        return template.render({"persons": self._extract_data(), "date": self.date_str})
 
 
-def scrape() -> "Iterator[Dict[str, Union[str, int]]]":
-    r = requests.get(SWEDEN_TOPLIST_URL)
-    soup = BeautifulSoup(r.content, "html.parser")
-    # MrKoll's HTML is broken (the p.infoLine2 tags are not closed)
-    person_list = soup.select(".infoLine2 a")
-    for person in person_list:
-        name, location = person.select_one("span.topp1").contents
-        name = name.text
-        location = re.sub(r"^\si\s", "", location)
-        search_term = make_search_term(name, location)
-        result = {
-            "name": name,
-            "location": location,
-            "search_count": int(re.sub(r"\D", "", person.select_one("span.topp2").text)),
-            "mrkoll_link": urljoin(MRKOLL_BASE_URL, person.attrs["href"]),
-            "flashback_link": FLASHBACK_BASE_URL + search_term,
-            "ddg_link": DDG_BASE_URL + search_term,
-            "facebook_link": FACEBOOK_BASE_URL + make_search_term(name),
-        }
-        yield result
-
-
-def get_current_toplist_html() -> str:
-    jinja = Environment(loader=PackageLoader("mrks", "templates"), autoescape=select_autoescape(["html"]))
-    template = jinja.get_template("entry.html")
-    return template.render({"persons": scrape(), "date": date.today()})
-
-
-def save_current_entry():
-    fe = FeedEntry()
-    fe.title(str(date.today()))
-    fe.published(datetime.now(tz=gettz("Europe/Stockholm")))
-    fe.description(get_current_toplist_html())
-    with shelve.open(SHELVE_DB, writeback=True) as db:
-        if "entries" not in db:
-            db["entries"] = []
-        db["entries"].append(fe)
-
-
-def get_entries() -> "List[FeedEntry]":
+def get_entries() -> "List[Entry]":
     with shelve.open(SHELVE_DB) as db:
         if "entries" in db:
-            return db["entries"]
+            return db["entries"].values()
         return []
 
 
@@ -84,9 +102,19 @@ def generate_rss() -> bytes:
     fg.description("An weekly RSS feed of mrkoll.se's Swedish toplist")
 
     for entry in get_entries():
-        fg.add_entry(entry)
+        fg.add_entry(entry.feed_entry)
 
     return fg.rss_str(pretty=True)
+
+
+def regenerate():
+    for entry in get_entries():
+        entry.generate_feed_entry()
+        entry.save()
+
+
+def scrape():
+    Entry.fetch_and_save()
 
 
 if __name__ == "__main__":
